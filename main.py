781fadddfc93d8235da3d2f3b203546f9efd4535
legendarybbk#5503 <72668615+legendarybbk5503@users.noreply.github.com>
2023-01-28 16:48:05 +0000

    .

---
+++ main.py
@@ -0,0 +1,8 @@
+from projectile import projectile
+
+#environment constant
+x = projectile(m = 15e-3, r = 0.01508, C = 0.75, rho = 1.293, g = 9.80665)
+#get recorded data
+x.getData("data.txt")
+#get graph (x-axis, y-axis)
+x.draw('t', 'ay')
