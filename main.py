3d72db4c4149573ce919661415bf0c13db29ff05
legendarybbk#5503 <72668615+legendarybbk5503@users.noreply.github.com>
2023-01-30 19:42:37 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ x = projectile(m = 15e-3, r = 0.01508, C = 0.75, rho = 1.293, g = 9.80665)
 x.getData("data.txt")
 #get graph (x-axis, y-axis)
 #choices: t, x, y, v, vx, vy, a, ax, ay
-x.draw('t', 'v')
+x.draw('x', 'y')
